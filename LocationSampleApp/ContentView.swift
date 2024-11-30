3fd911353bd7c49ece658dc3bed6dfc970a58511
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-30 23:27:02 +0100

    Sample code implemented

--- LocationSampleApp/ContentView.swift
+++ LocationSampleApp/ContentView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            if let location = locationManager.currentLocation {
+                Text("Latitude: \(location.latitude)")
+                Text("Longitude: \(location.longitude)")
+            } else {
+                Text("Location not available")
+            }
+            
+            if let address = locationManager.currentAddress {
+                Text("Name: \(address.name ?? "Unknown")")
+                Text("Town: \(address.locality ?? "Unknown")")
+                Text("Country: \(address.country ?? "Unknown")")
+            } else {
+                Text("Address not available")
+            }
+            
+            Button(action: {
+                locationManager.requestLocation()
+            }) {
+                Text("Request Location")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .onAppear {
+            locationManager.checkAuthorization()
         }
         .padding()
     }
@@ -22,3 +47,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
---
+++ LocationSampleApp/LocationManager.swift
@@ -0,0 +1,70 @@
+//
+//  LocationManager.swift
+//  LocationSampleApp
+//
+//  Created by Javier Calatrava on 30/11/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private var locationManager = CLLocationManager()
+    private let geocoder = CLGeocoder()
+    
+    @Published var currentLocation: CLLocationCoordinate2D?
+    @Published var currentAddress: CLPlacemark?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("Location access denied")
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.requestLocation()
+        @unknown default:
+            break
+        }
+    }
+    
+    func requestLocation() {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAuthorization()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.currentLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                          longitude: location.coordinate.longitude)
+            reverseGeocode(location: location)
+        }
+    }
+    
+    private func reverseGeocode(location: CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first, error == nil {
+                self?.currentAddress = CLPlacemark(placemark: placemark)
+            } else {
+                print("Error during reverse geocoding: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+}
+
