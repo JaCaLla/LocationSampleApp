1ba5f7070691bd43249e0dedcd8558582855aa0d
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-30 21:24:53 +0100

    Add Blank iOS App project

---
+++ LocationSampleApp/LocationSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSampleAppApp.swift
+//  LocationSampleApp
+//
+//  Created by Javier Calatrava on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
